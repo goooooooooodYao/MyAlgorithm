0f20bbbf1e2dac7c0c0f013fa06838e3c3c867f9
yaowei <yaowei0501@icloud.com>
2020-04-17 11:19:52 +0800

    first commit

---
+++ MyAlgorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyAlgorithm
+//
+//  Created by yaowei1314cj on 2020/4/17.
+//  Copyright © 2020 thirdProject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
